4f69d0dc33b5c783b08d115610de8dbb0012327c
SanishPoudel <mr.sanish.poudel@gmail.com>
2024-03-01 23:26:32 +1100

    Creates a new grid everytime you resize the gird now.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// Create divs and add them to the div container.
-// Set hovering effect for the divs.
-// Add a button for the prompt that will ask about the number of squares in a grid.
-// When a number is added into the prompt delete the existing grid and create a new grid.
-
 const button = document.querySelector("button");
 const container = document.querySelector(".container");
 
+// Add a button for the prompt that will ask about the number of squares in a grid.
 button.addEventListener
 (
     "click", () =>
     {
+        // When a number is added into the prompt delete the existing grid and create a new grid.
+        while (container.firstChild)
+        {
+            container.removeChild(container.lastChild);
+        }
+
         let squares_per_side = prompt("Enter number of squares per side for the new grid.");
         while (squares_per_side > 100)
         {
@@ -20,6 +22,7 @@ button.addEventListener
 
         for (let i = 0; i < gridSquares; i++)
         {
+            // Create divs and add them to the div container.
             let new_div = document.createElement("div");
             new_div.textContent = "I'm a div."
             container.appendChild(new_div);
